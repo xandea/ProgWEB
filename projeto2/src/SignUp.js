d7a04be885fc2cbab974028533a9127783526f94
xandea <xandeebr@hotmail.com>
2021-04-06 12:09:54 -0300

    Hooks react para login e função de onChange

--- projeto2/src/SignUp.js
+++ projeto2/src/SignUp.js
@@ -1,24 +1,63 @@
-import { render } from '@testing-library/react'
-import React, { Component } from 'react'
+import { render } from '@testing-library/react';
+import React, { Component, useState } from 'react';
 
-class SignUp extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-          email:'',
-          password:''
-        };
-      }
+    function Example() {
+        // Declaração das variaveis globais
+        const [email, setEmail] = useState("")
+        const [password, setPassword] = useState("")
 
+        const onChangeEmail = (e) => {
+            const emailValeu = e.target.value
+            setEmail(emailValeu)
+        }
+        const onChangePassword = (e) => {
+            const passwordValue = e.target.value
+            setPassword(passwordValue)
+        }
+
+        console.log(email)
+        console.log(password)
+
+        return(
+            <container>
+                <div>
+                    <input 
+                    type="email" 
+                    name="user"
+                    placeholder="e-mail"
+                    onChange={onChangeEmail}
+                    />
+                </div>
+                <div>
+                    <input 
+                    type="text" 
+                    name="senha"
+                    placeholder="senha"
+                    onChange={onChangePassword}
+                    />
+                </div>
+                <button>Enviar</button>
+            </container>
+        )
+}
+
+
+
+    
+    
+
+    
+/*
     render(){
         return(
             <container>
                 <div>
                     <input 
                     type="email" 
-                    name="usuario"
+                    name="user"
                     placeholder="e-mail"
                     value={this.state.email}
+                    onChange={this.setEmail}
                     />
                 </div>
                 <div>
@@ -34,5 +73,5 @@ class SignUp extends React.Component{
         )
     }
 }
-
-export default SignUp;
+*/
+export default Example;
