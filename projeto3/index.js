cdc1d84c8fb3929d800a20c4633a30e849e32f12
xandea <xandeebr@hotmail.com>
2021-05-15 16:01:31 -0300

    Conexao com banco mongodb atlas

--- projeto3/index.js
+++ projeto3/index.js
@@ -1,16 +1,17 @@
 let http = require('http'),
     path = require('path'),
     express = require('express'),
-    app = express();
+    app = express(),
+    db = require('./mvc/model/db');
 
     app.set('view engine', 'hbs');
-    app.set('views', path.join(__dirname, 'projeto3/view'));
+    app.set('views', path.join(__dirname, 'mvc/view'));
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.urlencoded({extended:false}));
+    
 
-    app.get('/pato', (req,res)=>{
-        res.render('Pato');
-
+    app.get('/', (req,res)=>{
+        res.render('login.hbs')
     });
 
 
---
+++ projeto3/mvc/model/db.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alexandre:teste123@projeto.cqxxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+module.exports = MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("exchange");
+
+    dbo.collection("user").findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+});
