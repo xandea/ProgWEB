01ff05c2104d8d3fa7793b03abd09e9babc92537
xandea <xandeebr@hotmail.com>
2021-05-20 21:01:36 -0300

    Alteração na axios de login

--- projeto2/src/pages/Login/SignIn.js
+++ projeto2/src/pages/Login/SignIn.js
@@ -28,7 +28,7 @@ function Example() {
 
         if (password!=="" && emailRegex.test(email)){
             try {           
-                await axios.post(process.env.REACT_APP_API_URL,{
+                await axios.post(process.env.REACT_APP_API_URL+"/login",{
                 email: email,//"eve.holt@reqres.in",
                 senha: password//"cityslicka"
                 })
